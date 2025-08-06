7e60db2e3ad25b890cb6c03a56e84b20e9e58d55
syhamm830 <153018200+syhamm830@users.noreply.github.com>
2025-08-06 20:09:40 +0100

    Delete logger.py

--- logger.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-tstamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-logfile = f'{tstamp}_log.txt'
-
-def writehistory(text):
-    with open(logfile, 'a', encoding='utf-8') as f:
-        f.write(text + '\n')
